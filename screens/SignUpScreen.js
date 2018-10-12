4baae59fed9a941b331e82f44aeed977bc863d53
Max Camm <mcammsoccer@yahoo.com>
2018-10-11 21:50:21 -0400

    Added Form to sign up screen

--- screens/SignUpScreen.js
+++ screens/SignUpScreen.js
@@ -1,6 +1,6 @@
 import React from 'react';
-import {Image, Platform, ScrollView, StyleSheet, Text, TouchableOpacity, View } from 'react-native';
-import { Button, Card, FormInput } from 'react-native-elements';
+import {Image, Platform, ScrollView, StyleSheet, Text, TextInput, TouchableOpacity, View } from 'react-native';
+import { Icon, Button, Card, FormInput, FormLabel, FormValidationMessage } from 'react-native-elements';
 
 
 export default class SignUpScreen extends React.Component {
@@ -10,8 +10,24 @@ export default class SignUpScreen extends React.Component {
 
       render(){
         return(
-          <View style={styles.stubContainer}>
-            <Text style={styles.stub}>WHAT THE HECK</Text>
+          <View style={styles.formContainer}>
+            <FormLabel>Name</FormLabel> 
+            <FormInput onChangeText = {(text) => this.setState({text})}/>
+            <FormLabel>Username</FormLabel>
+            <FormInput onChangeText = {(text) => this.setState({text})}/>
+            <FormLabel>Password</FormLabel>
+            <FormInput onChangeText = {(text) => this.setState({text})}/>
+          
+            <View style = {styles.fButtonContainer}>
+              <Button
+                raised
+                backgroundColor = 'rgba(170, 10, 10, 1)'
+                buttonStyle={{ marginTop: 20 }}
+                title = 'Submit!'
+                fontSize = {36}
+
+                />
+            </View>
           </View>
 
         );
@@ -26,14 +42,15 @@ const styles = StyleSheet.create({
     color: 'rgb(0, 0, 0)',
   },
   stubContainer: {
-    backgroundColor: 'rgb(255, 255, 255)',    
+    backgroundColor: '#AA000C',    
+  },
+  formContainer: {
+    flex: 1,
+    padding: 30,
+    backgroundColor: '#303030',
+  },
+  fButtonContainer: {
+    backgroundColor: '#303030',
   }
-
-
-
-
-
-
-
 
 })
